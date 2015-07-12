ae05a2ca82242c8e428ab41d7191828d4fae8cc6
hatz48 <hatz48@hatena.ne.jp>
2015-07-12 18:35:37 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Promise::Tiny - It's new $module
+
+# SYNOPSIS
+
+    use Promise::Tiny;
+
+# DESCRIPTION
+
+Promise::Tiny is ...
+
+# LICENSE
+
+Copyright (C) hatz48.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+hatz48 <hatz48@hatena.ne.jp>
---
+++ lib/Promise/Tiny.pm
@@ -0,0 +1,39 @@
+package Promise::Tiny;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Promise::Tiny - It's new $module
+
+=head1 SYNOPSIS
+
+    use Promise::Tiny;
+
+=head1 DESCRIPTION
+
+Promise::Tiny is ...
+
+=head1 LICENSE
+
+Copyright (C) hatz48.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+hatz48 E<lt>hatz48@hatena.ne.jpE<gt>
+
+=cut
+
